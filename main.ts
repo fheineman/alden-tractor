dfba9eb101f471a0969f25b4b0a8475db5e65cd3
fheineman <fheineman@me.com>
2021-06-02 16:12:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ function goBackward () {
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.analogWritePin(AnalogPin.P1, speed / 4)
+    pins.analogWritePin(AnalogPin.P1, speed)
 }
 function goForward () {
     pins.digitalWritePin(DigitalPin.P8, 1)
@@ -10,6 +10,9 @@ function goForward () {
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.analogWritePin(AnalogPin.P1, speed)
 }
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C C D D D D C C ", 480)
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "joyH") {
         joyH = value
@@ -20,20 +23,20 @@ radio.onReceivedValue(function (name, value) {
 function Stop () {
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    speed = 0
-    pins.analogWritePin(AnalogPin.P1, speed)
+    pins.analogWritePin(AnalogPin.P1, speed / 2)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-let enableRC = 0
-let footPedal2 = 0
-let enableAdult = 0
 let seatSwitch = 0
+let enableRC = 0
+let enableAdult = 0
+let footPedal2 = 0
 let footPedal1 = 0
 let speed = 0
 let joyV = 0
 let joyH = 0
 led.enable(false)
 Stop()
+pins.analogSetPeriod(AnalogPin.P1, 20000)
 pins.setPull(DigitalPin.P7, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P9, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
@@ -45,38 +48,7 @@ joyV = 512
 music.playMelody("C E G C5 - G C5 C5 ", 360)
 music.playTone(523, music.beat(BeatFraction.Breve))
 music.setTempo(120)
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P15) != direction && footPedal1 < 50) {
-        direction = pins.digitalReadPin(DigitalPin.P15)
-    }
-    if (direction == 1) {
-        if (footPedal1 > 50 && seatSwitch == 1) {
-            speed = footPedal1
-            goForward()
-        } else if (enableAdult == 1 && footPedal2 > 50 && seatSwitch == 1) {
-            speed = footPedal2
-            goForward()
-        } else if (enableRC == 1 && joyV > 550) {
-            speed = Math.map(joyV, 550, 1023, 10, 255)
-            goForward()
-        } else {
-            Stop()
-        }
-    } else {
-        if (footPedal1 > 50 && seatSwitch == 1) {
-            speed = footPedal1
-            goBackward()
-        } else if (enableAdult == 1 && footPedal2 > 50 && seatSwitch == 1) {
-            speed = footPedal2
-            goBackward()
-        } else if (enableRC == 1 && joyV < 450) {
-            speed = Math.map(joyV, 0, 450, 255, 10)
-            goBackward()
-        } else {
-            Stop()
-        }
-    }
-})
+music.setBuiltInSpeakerEnabled(true)
 basic.forever(function () {
     if (direction == 0) {
         music.playTone(988, music.beat(BeatFraction.Whole))
@@ -90,4 +62,46 @@ basic.forever(function () {
     enableRC = pins.digitalReadPin(DigitalPin.P9)
     direction = pins.digitalReadPin(DigitalPin.P15)
     seatSwitch = pins.digitalReadPin(DigitalPin.P16)
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P15) != direction && footPedal1 < 50) {
+        direction = pins.digitalReadPin(DigitalPin.P15)
+    }
+})
+basic.forever(function () {
+    if (seatSwitch == 0) {
+        if (enableRC == 1 && joyV > 550) {
+            speed = Math.map(joyV, 550, 1023, 10, 1023)
+            goForward()
+        } else if (enableRC == 1 && joyV < 450) {
+            speed = Math.map(joyV, 0, 450, 1023, 10)
+            goBackward()
+        } else {
+            speed = 0
+            pins.analogWritePin(AnalogPin.P1, speed)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+        }
+    } else {
+        if (direction == 1) {
+            if (footPedal1 > 50 && seatSwitch == 1) {
+                speed = footPedal1
+                goForward()
+            } else if (enableAdult == 1 && footPedal2 > 50 && seatSwitch == 1) {
+                speed = footPedal2
+                goForward()
+            } else {
+                Stop()
+            }
+        } else {
+            if (footPedal1 > 50 && seatSwitch == 1) {
+                speed = footPedal1
+                goBackward()
+            } else if (enableAdult == 1 && footPedal2 > 50 && seatSwitch == 1) {
+                speed = footPedal2
+                goBackward()
+            } else {
+                Stop()
+            }
+        }
+    }
 })
